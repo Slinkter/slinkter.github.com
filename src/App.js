cc9806484a90e986f6b9019e2c5081e60fd1d557
Slinkter <luis.j.cueva@gmail.com>
2024-01-13 14:50:45 -0500

    revision

--- src/App.js
+++ src/App.js
@@ -6,29 +6,28 @@ import {
   Typography,
   Button,
 } from "@material-tailwind/react";
+import { ComplexNavbar } from "./Components/Nav";
+import { CardDefault } from "./Components/Card";
 
 export default function App() {
   return (
-    <Card className="mt-6 w-96">
-      <CardHeader color="blue-gray" className="relative h-56">
-        <img
-          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-          alt="card-image"
-        />
-      </CardHeader>
-      <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          UI/UX Review Check
-        </Typography>
-        <Typography>
-          The place is close to Barceloneta Beach and bus stop just 2 min by
-          walk and near to &quot;Naviglio&quot; where you can enjoy the main
-          night life in Barcelona.
-        </Typography>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Button>Read More</Button>
-      </CardFooter>
-    </Card>
+    <div>
+      <ComplexNavbar></ComplexNavbar>
+
+      <div className=" grid grid-cols-1  lg:grid-cols-3 gap-3 justify-center items-center">
+        <CardDefault />
+        <CardDefault />
+        <CardDefault />
+        <CardDefault />
+        <CardDefault />
+        <CardDefault />
+        <CardDefault />
+        <CardDefault />
+        <CardDefault />
+        <CardDefault />
+        <CardDefault />
+        <CardDefault />
+      </div>
+    </div>
   );
 }
