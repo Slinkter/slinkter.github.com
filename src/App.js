a8a544ca64d959fd162d20dd7c7267d4bd2d02cc
Slinkter <luis.j.cueva@gmail.com>
2024-05-15 16:27:14 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Typography,
-  Button,
-} from "@material-tailwind/react";
-import { ComplexNavbar } from "./Components/Nav";
-import { CardDefault } from "./Components/Card";
+import { Typography } from "@material-tailwind/react";
 
 export default function App() {
   return (
-    <div>
-      <ComplexNavbar></ComplexNavbar>
-
-      <div className=" grid grid-cols-1  lg:grid-cols-3 gap-3 justify-center items-center">
-        <CardDefault />
-        <CardDefault />
-        <CardDefault />
-        <CardDefault />
-        <CardDefault />
-        <CardDefault />
-        <CardDefault />
-        <CardDefault />
-        <CardDefault />
-        <CardDefault />
-        <CardDefault />
-        <CardDefault />
-      </div>
-    </div>
+    <>
+      <Typography variant="h1">Hola mundo</Typography>
+      <Typography variant="h2" color="red">
+        Hola mundo
+      </Typography>
+    </>
   );
 }
