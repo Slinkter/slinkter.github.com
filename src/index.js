408928dfa5e8eafba627f73f6eb22f16d7ced5e2
Slinkter <luis.j.cueva@gmail.com>
2024-05-17 17:45:29 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-import { ThemeProvider } from "@material-tailwind/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
-);
+root.render(<App />);
