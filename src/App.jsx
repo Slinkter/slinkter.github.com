8207c8e95918054fe3175848f74cf3ab5b831a88
Slinkter <luis.j.cueva@gmail.com>
2024-09-27 18:49:52 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import { useContext } from "react";
 import { CustomThemeContext } from "./CustomThemeContext";
 import { FaSun, FaMoon } from "react-icons/fa";
+import { Button } from "@material-tailwind/react";
 
 function App() {
     const { theme, toggleTheme } = useContext(CustomThemeContext);
@@ -89,6 +90,30 @@ function App() {
                         Habilidades
                     </h2>
                     <Skills />
+
+                    {/* Botón para descargar el CV */}
+
+                    <a
+                        href="/cv-luis-cueva.pdf"
+                        download
+                        className="mt-12 p-3 bg-blue-500 text-white rounded-md shadow-md hover:bg-blue-700 transition duration-300 inline-flex items-center"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={2}
+                            stroke="currentColor"
+                            className="h-5 w-5 mr-2"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+                            />
+                        </svg>
+                        Descargar CV
+                    </a>
 
                     {/* Botón para cambiar el tema */}
                 </div>
