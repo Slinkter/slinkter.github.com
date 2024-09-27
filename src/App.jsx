f53eb430126eb7f27b21e2cf01979ff7daf13330
Slinkter <luis.j.cueva@gmail.com>
2024-09-27 12:50:07 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,65 +9,68 @@ import { FaSun, FaMoon } from "react-icons/fa";
 function App() {
     const { theme, toggleTheme } = useContext(CustomThemeContext);
 
+    const url =
+        "https://raw.githubusercontent.com/Slinkter/slinkter.github.com/refs/heads/main";
+
     const db_projectbasics = [
         {
             name: "Project 01 ",
-            image: "/src/assets/api01.png",
+            image: url + "/src/assets/api01.png",
             link: "https://slinkter.github.io/myprojectapi01/",
         },
         {
             name: "Project 02 ",
-            image: "/src/assets/api02.png",
+            image: url + "/src/assets/api02.png",
             link: "https://slinkter.github.io/myprojectapi02/",
         },
         {
             name: "Project 03 ",
-            image: "/src/assets/api03.png",
+            image: url + "/src/assets/api03.png",
             link: "https://slinkter.github.io/myprojectapi03/",
         },
         {
             name: "Project 04 ",
-            image: "/src/assets/api04.png",
+            image: url + "/src/assets/api04.png",
             link: "https://slinkter.github.io/myprojectapi04/",
         },
         {
             name: "Project 05 ",
-            image: "/src/assets/api05.png",
+            image: url + "/src/assets/api05.png",
             link: "https://slinkter.github.io/myprojectapi05/",
         },
         {
             name: "Project 06 ",
-            image: "/src/assets/api07.png",
+            image: url + "/src/assets/api07.png",
             link: "https://slinkter.github.io/myprojectapi06/",
         },
         {
             name: "Project 07 ",
-            image: "/src/assets/api01.png",
+            image: url + "/src/assets/api01.png",
             link: "https://slinkter.github.io/myprojectapi07/",
         },
         {
             name: "Project 08 ",
-            image: "/src/assets/api08.png",
+            image: url + "/src/assets/api08.png",
             link: "https://slinkter.github.io/myprojectapi08/",
         },
         {
             name: "Project 09 ",
-            image: "/src/assets/api09.png",
+            image: url + "/src/assets/api09.png",
             link: "https://slinkter.github.io/myprojectapi09/",
         },
         {
             name: "Project 10 ",
-            image: "/src/assets/api10.png",
+            image: url + "/src/assets/api10.png",
             link: "https://slinkter.github.io/myprojectapi10/",
         },
         {
             name: "Project 11 ",
-            image: "/src/assets/api11.png",
+            image: url + "/src/assets/api11.png",
             link: "https://slinkter.github.io/myprojectapi11/",
         },
         {
             name: "Project 12 ",
-            image: "/src/assets/api12.png",
+            image: url + "/src/assets/api12.png",
             link: "https://slinkter.github.io/myprojectapi12/",
         },
     ];
